04fcf12bc87a6ddab549aa2bf0bba4dd914e3a59
Dmytro Deinershtein <dmytro_deinershtein@epam.com>
2021-03-21 20:01:43 +0200

    rebuild api

--- movie-blog/src/components/components/contained/Catalog.js
+++ movie-blog/src/components/components/contained/Catalog.js
@@ -1,9 +1,21 @@
 import React, {Component, useState, useEffect} from 'react';
-import Container from "../../elements/Container";
 import Label from "../../elements/Label";
 import Item from "./Item";
 
 const Catalog = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  const apiBaseURL = 'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/',
+    headers = {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "647a93cc65msh50494ccaf2a1f63p19a4dajsnd4639551f841",
+        "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com"
+      }
+    };
+
   const movies = [
     {slug: "knocking on heaven's door", genre: ['drama', 'criminal']},
     {slug: "interstellar", genre: ['sci-fi', 'drama']},
@@ -13,38 +25,40 @@ const Catalog = () => {
     {slug: "Showshank redemption", genre: ['drama', 'criminal']}
   ]
 
-  // dynamically populated list of the genres, based on the movies array
-
-  const genres = ['all',...new Set(movies.flatMap(({genre}) => genre))]
-
-  const attributes = ['none', 'slug']
-
-  const [needle, setNeedle] = useState(),
-    [activeGenre, setActiveGenre] = useState('all'),
-    [orderAttribute, setOrderAttribute] = useState('none'),
-    onSearch = ({target:{value}}) => setNeedle(value),
-    isOfGenre = genres => genres.includes(activeGenre) || activeGenre === 'all',
-    onPickGenre = newActiveGenre => setActiveGenre(newActiveGenre),
-    onOrderPick = attr => setOrderAttribute(attr)
+  movies.map(({slug, genre}) => (
+    useEffect(() => {
+      fetch(apiBaseURL + encodeURI(slug), headers)
+        .then(res => res.json())
+        .then((res) => {
+            res.genre = genre;
+            setItems(items => [...items, res]);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  ));
 
   return (
     <div className={'content__contained catalog'}>
       <Label className={'results-found'} text={'39 movies found'}/>
       <div className={'item-listing'}>
         {
-          movies
-            .sort(({[orderAttribute]:a},{[orderAttribute]:b}) =>
-              orderAttribute !== 'none' && a && b && a.localeCompare(b))
-            .map(({slug, genre}) => isOfGenre(genre) && (
-              <Item
-                key={slug}
-                slug={slug}
-                needle={needle}
-                genre={genre}
-              />
-            ))
+          items.map((item, index) => {
+            return (
+                <Item
+                  key={index}
+                  poster={item.poster}
+                  title={item.title}
+                  name={item.name}
+                  year={item.year}
+                  genre={item.genre}
+                />
+              )
+          })
         }
-
       </div>
     </div>
   )
--- movie-blog/src/components/components/contained/Item.js
+++ movie-blog/src/components/components/contained/Item.js
@@ -1,52 +1,20 @@
-import React, {Component, useState, useEffect} from 'react';
+import React from 'react';
 import Button from "../../elements/Button";
 
-const Item = ({slug, needle, genre}) => {
-  // movieDetails object is undefined upon initialization,
-  // after component is mounted, useEffect() hook fetches
-  // the data from API and populates all the necessary details
-
-  const [movieDetails, setMovieDetails] = useState(),
-    hasNeedle = (s = '') =>
-      !!s && Object
-        .values(movieDetails || {})
-        .some(movieDetail =>
-          movieDetail
-            .toLowerCase()
-            .includes(s.toLowerCase()))
-
-  // API params are appended with the slug (search keywords)
-
-  const apiBaseURL = 'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/',
-    headers = {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "647a93cc65msh50494ccaf2a1f63p19a4dajsnd4639551f841",
-        "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com"
-      }
-    }
-
-  useEffect(() => {
-    fetch(apiBaseURL+encodeURI(slug), headers)
-      .then(res => res.json())
-      .then(({title, poster, plot, year}) =>
-        setMovieDetails({title, poster, plot, year})
-      )
-  }, [])
-
-  return !!movieDetails && (!needle || hasNeedle(needle)) && (
+const Item = ({title, genre, year, poster}) => {
+  return (
     <div className={'item'}>
-      <div className={'item__cover'} style={{ backgroundImage: `url(${movieDetails.poster})`}}>
+      <div className={'item__cover'} style={{ backgroundImage: `url(${poster})`}}>
         <Button text={'item__menu'}/>
       </div>
       <div className={'item__description'}>
-        <h3 className={'item__name'}>{movieDetails.title}</h3>
+        <h3 className={'item__name'}>{title}</h3>
         <p className={'item__category'}>
-          {genre.map((name) => (
-              <span key={name}>{name}</span>
+          {genre.map((name, index) => (
+              <span key={index}>{name} </span>
           ))}
         </p>
-        <span className={'item__year'}>{movieDetails.year}</span>
+        <span className={'item__year'}>{year}</span>
       </div>
     </div>
   )
